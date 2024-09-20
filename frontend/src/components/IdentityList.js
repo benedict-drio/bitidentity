a413bf7ba2b3bdbf2b99ee92e961f62c32dd18b4
benedict-drio <nicholasbenedict04@gmail.com>
2024-09-20 14:52:35 +0100

    feat: Add IdentityList component

---
+++ frontend/src/components/IdentityList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const IdentityList = () => {
+  return (
+    <ul>
+      {/* List of identities will be rendered here */}
+    </ul>
+  );
+};
+
+export default IdentityList;
