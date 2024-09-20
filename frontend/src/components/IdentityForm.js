1b5cd7680671e8a1d2ed462e96673ff9187a3416
benedict-drio <nicholasbenedict04@gmail.com>
2024-09-20 14:23:01 +0100

    Add IdentityForm component

---
+++ frontend/src/components/IdentityForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const IdentityForm = () => {
+	return (
+		<form>
+			<input type="text" placeholder="Enter identity" />
+			<button type="submit">Create Identity</button>
+		</form>
+	);
+};
+
+export default IdentityForm;
