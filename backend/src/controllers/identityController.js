694714332315b5126b0b677ad37cd073dd15c794
benedict-drio <nicholasbenedict04@gmail.com>
2024-09-20 11:59:22 +0100

    Refactor identityController to use encryptionService for encrypting and decrypting identity data

--- backend/src/controllers/identityController.js
+++ backend/src/controllers/identityController.js
@@ -1,8 +1,14 @@
 const stacksService = require('../services/stacksService');
+const jwt = require('jsonwebtoken');
+const encryptionService = require('../services/encryptionService');
+
+const secretKey = 'your_jwt_secret';
 
 exports.createIdentity = async (req, res) => {
 	const { identity } = req.body;
 	try {
+		// Encrypt identity data before saving
+		const encryptedIdentity = encryptionService.encrypt(identity);
 		await stacksService.createIdentity(identity);
 		res.status(201).send('Identity created');
 	} catch (error) {
@@ -14,6 +20,8 @@ exports.getIdentity = async (req, res) => {
 	const { id } = req.params;
 	try {
 		const identity = await stacksService.getIdentity(id);
+		// Decrypt identity data before sending
+		const decryptedIdentity = encryptionService.decrypt(identity.encryptedData, identity.iv);
 		res.status(200).json(identity);
 	} catch (error) {
 		res.status(500).send(error.message);
