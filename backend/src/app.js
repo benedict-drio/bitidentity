87d51d9f9b49f57f014a4517d3b59b8a9ef18388
benedict-drio <nicholasbenedict04@gmail.com>
2024-09-20 11:59:36 +0100

    Refactor app.js to add a route for the root URL

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,6 +6,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use('/api/identities', identityRoutes);
 
+// Add a route to handle the root URL
+app.get('/', (req, res) => {
+	res.send('Welcome to the Identity API');
+});
+
 app.listen(3000, () => {
 	console.log('Server running on port 3000');
 });
